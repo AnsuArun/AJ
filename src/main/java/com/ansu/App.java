5f79069bfef7174c8eda6833a0073846d366c606
AnsuJacob <ansuannajacob29@gmail.com>
2022-06-29 06:55:53 +0000

    first commit

---
+++ src/main/java/com/ansu/App.java
@@ -0,0 +1,13 @@
+package com.ansu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
